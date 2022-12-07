1977e083537db45bc24f0d2b93e2d6934e91a648
子斗子 <1662093634@qq.com>
2022-12-07 17:01:42 +0800

    部分修改, 美化界面

--- app/src/main/java/zdz/openmyurl/MainActivity.kt
+++ app/src/main/java/zdz/openmyurl/MainActivity.kt
@@ -1,5 +1,6 @@
 package zdz.openmyurl
 
+import android.app.Activity
 import android.content.Intent
 import android.content.Intent.EXTRA_PROCESS_TEXT
 import android.net.Uri
@@ -7,21 +8,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,22 +47,32 @@ import java.net.URL
 class MainActivity : ComponentActivity() {
     
     private val vm: MainViewModel by viewModels()
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OpenMyURLTheme {
                 // A surface container using the 'background' color from the theme
+                
                 LazyColumn(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.padding(24.dp)
                 ) {
                     items(vm.list.size) {
+                        if (it != 0) {
+                            Spacer(
+                                modifier = Modifier
+                                    .height(24.dp)
+                                    .fillMaxWidth()
+                            )
+                        }
                         Card(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .clickable { openURL(vm.list[it]) }
+                                .clickable { openURL(vm.list[it]) },
+                            colors = if (it % 2 == 0) CardDefaults.cardColors() else CardDefaults.cardColors(
+                                contentColor = MaterialTheme.colorScheme.secondary
+                            )
                         ) {
                             Row(
                                 horizontalArrangement = Arrangement.Center,
@@ -99,15 +116,14 @@ class MainActivity : ComponentActivity() {
     }
     
     private fun openURL(url: String) {
-        val uri = Uri.parse(url)
-        val intent = Intent()
-        intent.action = Intent.ACTION_VIEW
-        intent.data = uri
-        vm.viewModelScope.launch {
-            startActivity(intent)
-            delay(1000)
-            finishAndRemoveTask()
+        val intent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(url)
         }
+        val request = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) finishAndRemoveTask()
+        }
+        request.launch(intent)
     }
     
     companion object {
